2043e83fff6a1a0c314a335c8cdfb5ed5f66625c
MiguelVeganzones <veganzones28@gmail.com>
2024-10-29 19:42:17 +0100

    Updated utility

--- include/Utility/random.hpp
+++ include/Utility/random.hpp
@@ -1,6 +1,9 @@
 #ifndef INCLUDED_RANDOM_NUMBER_GENERATOR
 #define INCLUDED_RANDOM_NUMBER_GENERATOR
 
+#include "concepts.hpp"
+#include <concepts>
+#include <type_traits>
 #ifndef NOMINMAX
 #define NOMINMAX
 #endif
@@ -19,57 +22,17 @@
 namespace utility::random
 {
 
-template <std::floating_point F>
-class random
+template <utility::concepts::arithmetic T, typename Enables = void>
+class random;
+
+template <utility::concepts::arithmetic T>
+class random<T, std::enable_if_t<std::is_integral_v<T>>>
 {
 public:
-    using value_type = F;
+    using value_type = T;
 
 private:
-    [[nodiscard]]
-    static auto static_instance() noexcept -> random&
-    {
-        static random s_Random_engine(0);
-        return s_Random_engine;
-    }
-
-public:
-    static auto s_seed(unsigned int seed_ = std::random_device{}()) noexcept -> void
-    {
-        static_instance().seed_engine(seed_);
-    }
-
-    [[nodiscard]]
-    static auto s_randfloat() noexcept -> value_type
-    {
-        return static_instance().randfloat();
-    }
-
-    /// @brief Generates a uniform integral number in the range [min, max]
-    /// @tparam T Integral type
-    /// @param min Inclusive lower bound
-    /// @param max Inclusive upper bound
-    /// @return integral number of type T uniformly distributed in the range
-    /// [min, max]
-    template <std::integral T>
-    [[nodiscard]]
-    inline static auto s_randintegral(T min, T max) noexcept -> T
-    {
-        return static_instance().randintegral(min, max);
-    }
-
-    [[nodiscard]]
-    inline static auto s_randnormal(value_type avg, value_type stddev) noexcept
-        -> value_type
-    {
-        return static_instance().randnormal(avg, stddev);
-    }
-
-    [[nodiscard]]
-    inline static auto s_randnormal() noexcept -> value_type
-    {
-        return static_instance().randnormal();
-    }
+    using float_value_type = std::common_type_t<float, value_type>;
 
 public:
     random(unsigned int seed = std::random_device{}()) noexcept
@@ -77,58 +40,157 @@ public:
         seed_engine(seed);
     }
 
-    random(random const&) noexcept            = default;
-    random(random&&) noexcept                 = default;
-    random& operator=(random const&) noexcept = default;
-    random& operator=(random&&) noexcept      = default;
-    ~random() noexcept                        = default;
-
-    [[nodiscard]]
-    auto randfloat() noexcept -> value_type
-    {
-        return m_Uniform_real_(m_Random_engine_);
-    }
-
-    /// @brief Generates a uniform integral number in the range [min, max]
-    /// @tparam T Integral type
+    /// @brief Generates a number in the range [min, max] for integral types and [min,
+    /// max]
+    /// @tparam T Arithmetic type
     /// @param min Inclusive lower bound
     /// @param max Inclusive upper bound
-    /// @return integral number of type T uniformly distributed in the range
+    /// @return Number of type T uniformly distributed in the range
     /// [min, max]
-    template <std::integral T>
     [[nodiscard]]
-    auto randintegral(T min, T max) noexcept -> T
+    inline auto randrange(value_type min, value_type max) noexcept -> value_type
     {
+        using distribution_t = std::uniform_int_distribution<value_type>;
         assert(min <= max);
-        std::uniform_int_distribution<T> uniform_dist(min, max);
-        return uniform_dist(m_Random_engine_);
-    }
-
-    [[nodiscard]]
-    auto randnormal(float avg, float stddev) noexcept -> float
-    {
-        std::normal_distribution<float> n(avg, stddev);
-        return n(m_Random_engine_);
-    }
-
-    [[nodiscard]]
-    auto randnormal() noexcept -> float
-    {
-        return m_Default_normal_(m_Random_engine_);
+        distribution_t uniform_dist(min, max);
+        return uniform_dist(random_engine_);
     }
 
 private:
     inline auto seed_engine(unsigned int seed) noexcept -> void
     {
-        m_Random_engine_.seed(seed);
+        random_engine_.seed(seed);
     }
 
 private:
-    std::mt19937_64                            m_Random_engine_;
-    std::uniform_real_distribution<value_type> m_Uniform_real_ =
-        std::uniform_real_distribution<value_type>(0.f, 1.f);
-    std::normal_distribution<value_type> m_Default_normal_ =
-        std::normal_distribution<value_type>(0.f, 1.f);
+    std::mt19937_64 random_engine_;
+};
+
+template <utility::concepts::arithmetic T>
+class random<T, std::enable_if_t<std::is_floating_point_v<T>>>
+{
+public:
+    using value_type = T;
+
+private:
+    using float_value_type = std::common_type_t<float, value_type>;
+
+public:
+    random(unsigned int seed = std::random_device{}()) noexcept
+    {
+        seed_engine(seed);
+    }
+
+    [[nodiscard]]
+    inline auto randfloat() noexcept -> value_type
+        requires std::is_floating_point_v<value_type>
+    {
+        return uniform_real_(random_engine_);
+    }
+
+    /// @brief Generates a number in the range [min, max] for integral types and [min,
+    /// max) for floating-point types
+    /// @tparam T Arithmetic type
+    /// @param min Inclusive lower bound
+    /// @param max Inclusive upper bound
+    /// @return Number of type T uniformly distributed in the range
+    /// [min, max] for integral types and [min, max) for floating-point types
+    [[nodiscard]]
+    inline auto randrange(value_type min, value_type max) noexcept -> value_type
+    {
+        using distribution_t = std::uniform_real_distribution<value_type>;
+        assert(min <= max);
+        distribution_t uniform_dist(min, max);
+        return uniform_dist(random_engine_);
+    }
+
+    [[nodiscard]]
+    inline auto randnormal(value_type avg, value_type stddev) noexcept -> value_type
+        requires std::is_floating_point_v<value_type>
+    {
+        std::normal_distribution<value_type> n(avg, stddev);
+        return n(random_engine_);
+    }
+
+    [[nodiscard]]
+    inline auto randnormal() noexcept -> value_type
+        requires std::is_floating_point_v<value_type>
+    {
+        return default_normal_(random_engine_);
+    }
+
+private:
+    inline auto seed_engine(unsigned int seed) noexcept -> void
+    {
+        random_engine_.seed(seed);
+    }
+
+private:
+    std::mt19937_64                                  random_engine_;
+    std::uniform_real_distribution<float_value_type> uniform_real_ =
+        std::uniform_real_distribution<float_value_type>(
+            float_value_type{ 0 },
+            float_value_type{ 1 }
+        );
+    std::normal_distribution<float_value_type> default_normal_ =
+        std::normal_distribution<float_value_type>(
+            float_value_type{ 0 },
+            float_value_type{ 1 }
+        );
+};
+
+struct srandom
+{
+private:
+    template <utility::concepts::arithmetic T>
+    [[nodiscard]]
+    inline static auto static_instance() noexcept -> random<T>&
+    {
+        static random<T> instance;
+        return instance;
+    };
+
+public:
+    template <std::floating_point F>
+    inline static auto seed(unsigned int seed_ = std::random_device{}()) noexcept -> void
+    {
+        static_instance<F>().seed_engine(seed_);
+    }
+
+    template <std::floating_point F>
+    [[nodiscard]]
+    inline static auto randfloat() noexcept -> F
+    {
+        return static_instance<F>().randfloat();
+    }
+
+    /// @brief Generates a number in the range [min, max] for integral types and [min,
+    /// max) for floating-point types
+    /// @tparam T Arithmetic type
+    /// @param min Inclusive lower bound
+    /// @param max Inclusive/Exclusive upper bound
+    /// @return Number of type T uniformly distributed in the range
+    /// [min, max] for integral types and [min, max) for floating-point types
+    template <utility::concepts::arithmetic T>
+    [[nodiscard]]
+    inline static auto randrange(T min, T max) noexcept -> T
+    {
+        return static_instance<T>().randrange(min, max);
+    }
+
+    template <std::floating_point F>
+    [[nodiscard]]
+    inline static auto randnormal(F avg, F stddev) noexcept -> F
+    {
+        return static_instance<F>().randnormal(avg, stddev);
+    }
+
+    template <std::floating_point F>
+    [[nodiscard]]
+    inline static auto randnormal() noexcept -> F
+    {
+        return static_instance<F>().randnormal();
+    }
 };
 
 } // namespace utility::random
