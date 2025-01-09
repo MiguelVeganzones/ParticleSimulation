5f37e2e09d5d2cfd597133113b318e086829a912
pablo deike <pablodeike12@gmail.com>
2025-01-09 19:29:39 +0100

    more tests

--- tests/ndtree_tests.cpp
+++ tests/ndtree_tests.cpp
@@ -4,14 +4,12 @@
 #include <gtest/gtest.h>
 #include <iostream>
 
-TEST(TreeTests, TreePerformsCorrectSplit)
+TEST(TreeTests, TreeSplitsAndContainAllElements)
 {
-    // GTEST_SKIP() << "skipping tree tests";
-    static constexpr auto N       = 3;
-    using F                       = double;
-    using particle_t              = pm::particle::ndparticle<N, F>;
-    const std::size_t Tree_Fanout = 2;
-    // inline static constexpr auto s_tree_fanout = Tree_Fanout;
+    static constexpr auto N           = 3;
+    using F                           = double;
+    using particle_t                  = pm::particle::ndparticle<N, F>;
+    const std::size_t Tree_Fanout     = 2;
     const int         depth_t         = 4;
     const int         box_capacity_t  = 4;
     const float       universe_radius = 100.0;
@@ -19,7 +17,6 @@ TEST(TreeTests, TreePerformsCorrectSplit)
     auto particles = particle_factory::generate_particle_set<N, F>(size, universe_radius);
     auto tree_t =
         ndt::ndtree<Tree_Fanout, particle_t>(particles, depth_t, box_capacity_t);
-    std::cout << tree_t.box().boxes();
     const auto n_elements = tree_t.box().elements();
     ASSERT_EQ(n_elements, 10);
 }
