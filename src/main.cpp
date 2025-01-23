e5c7a129e7d71d31f34e00adf6285ec9a38b12f3
MiguelVeganzones <veganzones28@gmail.com>
2025-01-23 20:59:43 +0100

    Added benchmark simulation

--- src/main.cpp
+++ src/main.cpp
@@ -6,12 +6,15 @@
 #include "particle_interaction.hpp"
 #include "random_distributions.hpp"
 #include "simulation_config.hpp"
-#include "synthetic_clock.hpp"
 #include <array>
 #include <concepts>
 #include <cstdlib>
 #include <iostream>
 #include <vector>
+
+#define SEED1 104845342
+#define SEED2 982523355
+#define SEED3 223857254
 
 constexpr auto universe_radius = 10.0;
 
@@ -43,7 +46,7 @@ auto generate_particle_set(std::size_t size)
         using distribution_t = random_distribution<F, DistributionCategory::Exponential>;
         using param_type     = typename distribution_t::param_type;
         static const param_type params(0.001);
-        static distribution_t   d(params);
+        static distribution_t   d(params, SEED1);
         return d() * F{ 100 };
     };
 
@@ -51,11 +54,11 @@ auto generate_particle_set(std::size_t size)
         using distribution_a_t = random_distribution<F, DistributionCategory::Uniform>;
         using param_type_a     = typename distribution_a_t::param_type;
         static const param_type_a params_a(-universe_radius, universe_radius);
-        static distribution_a_t   d_a(params_a);
+        static distribution_a_t   d_a(params_a, SEED2);
         using distribution_b_t = random_distribution<F, DistributionCategory::Gamma>;
         using param_type_b     = typename distribution_b_t::param_type;
         static const param_type_b params_b(F{ 1 }, F{ 1 });
-        static distribution_b_t   d_b(params_b);
+        static distribution_b_t   d_b(params_b, SEED3);
         return d_a(); // + universe_radius;
     };
 
@@ -105,6 +108,43 @@ auto generate_charged_particle_set(std::size_t size)
     return particle_set_factory<N, F>(
         size, mass_generator, position_generator, velocity_generator, charge_generator
     );
+}
+
+int barnes_hut_bench()
+{
+    using namespace pm;
+    using F                    = double;
+    static constexpr auto N    = 3;
+    using particle_t           = particle::ndparticle<N, F>;
+    constexpr auto interaction = pm::interaction::InteractionType::Gravitational;
+
+#ifdef NDEBUG
+    const auto config_file_path = "data/input/release/config.ini";
+#else
+    const auto config_file_path = "data/input/debug/config.ini";
+#endif
+
+    const auto config    = simulation::config::parse_config<particle_t>(config_file_path);
+    const auto size      = config.general_config().particle_count_;
+    auto       particles = generate_particle_set<N, F>(size);
+
+    assert(config.is_valid());
+    config.print();
+    if (config.physics_config_.gravitational_constant_.has_value())
+    {
+        pm::physical_parameters<F>::set_gravitational_constant(
+            config.physics_config_.gravitational_constant_.value()
+        );
+    }
+
+    simulation::bh_approx::barnes_hut_approximation<particle_t, interaction> simulation_a(
+        particles, config.general_config(), config.barnes_hut_config()
+    );
+
+    std::cout << "Simulation A\n";
+    simulation_a.run();
+
+    return EXIT_SUCCESS;
 }
 
 int barnes_hut_test()
@@ -193,8 +233,8 @@ int main()
     utility::logging::default_source::log(
         utility::logging::severity_level::error, "Huge error or sth..."
     );
-    barnes_hut_test();
-    electrostatic_test();
+    barnes_hut_bench();
+    // electrostatic_test();
 
 #ifdef USE_ROOT_PLOTTING
     app.Run();
